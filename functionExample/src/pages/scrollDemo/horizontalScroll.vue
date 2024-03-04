97de29e3ef32718c82561268f69fca68e1eee4d3
zhangping <2425238116@qq.com>
2024-03-04 11:00:31 +0800

    feat: 横向滚动条，滚动到某一项的位置

---
+++ functionExample/src/pages/scrollDemo/horizontalScroll.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="box">
+        <ul class="box-list">
+            <li v-for="item in list" :key="item.id" class="box-list-item">{{ item.name }}</li>
+        </ul>
+        <Button @click="scrollToRight">滚动到最右侧</Button>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { Button } from 'view-ui-plus';
+
+const list = [
+    { id: 1, name: '选项1' },
+    { id: 2, name: '选项2' },
+    { id: 3, name: '选项3' },
+    { id: 4, name: '选项4' },
+    { id: 5, name: '选项5' },
+];
+
+// 滚动到最右
+const scrollToRight = () => {
+    const items = document.querySelectorAll('.box-list-item');
+    const scrollOptions = { behavior: 'smooth', block: 'nearest' };
+    if (items && items.length) {
+        items[items.length - 1].scrollIntoView(scrollOptions);
+    }
+};
+</script>
+
+<style scoped>
+.box {
+    .box-list {
+        border: 1px solid #000;
+        display: flex;
+        overflow-x: auto;
+        width: 200px;
+        padding: 5px 10px;
+        list-style: none;
+        .box-list-item {
+            padding: 5px 10px;
+            white-space: nowrap;
+            border: 1px solid #f00;
+        }
+    }
+}
+</style>
--- functionExample/src/router/index.js
+++ functionExample/src/router/index.js
@@ -1,42 +1,50 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
     {
-        path: "/",
-        name: "",
-        redirect: "/directive",
+        path: '/',
+        name: '',
+        redirect: '/directive',
     },
     {
         path: `/directive`,
-        name: "directive",
+        name: 'directive',
         meta: {
-            title: "指令页",
+            title: '指令页',
         },
-        component: () => import("@/pages/directiveDemo/index.vue"),
+        component: () => import('@/pages/directiveDemo/index.vue'),
     },
     {
         path: `/routerHome`,
-        name: "routerHome",
+        name: 'routerHome',
         meta: {
-            title: "路由-首页",
+            title: '路由-首页',
         },
-        component: () => import("@/pages/routerDemo/home.vue"),
+        component: () => import('@/pages/routerDemo/home.vue'),
     },
     {
         path: `/routerAbout`,
-        name: "routerAbout",
+        name: 'routerAbout',
         meta: {
-            title: "路由-关于",
+            title: '路由-关于',
         },
-        component: () => import("@/pages/routerDemo/about.vue"),
+        component: () => import('@/pages/routerDemo/about.vue'),
     },
     {
         path: `/plugin`,
-        name: "plugin",
+        name: 'plugin',
         meta: {
-            title: "自定义全局插件",
+            title: '自定义全局插件',
         },
-        component: () => import("@/pages/pluginDemo/index.vue"),
+        component: () => import('@/pages/pluginDemo/index.vue'),
+    },
+    {
+        path: `/scroll`,
+        name: 'scroll',
+        meta: {
+            title: '滚动',
+        },
+        component: () => import('@/pages/scrollDemo/horizontalScroll.vue'),
     },
 ];
 
