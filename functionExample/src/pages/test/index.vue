4eb62ac7f6b453736ab80448718042d7b1184764
zhangping <2425238116@qq.com>
2024-07-23 22:09:28 +0800

    chore: 测试

--- functionExample/src/pages/test/index.vue
+++ functionExample/src/pages/test/index.vue
@@ -1,9 +1,15 @@
 <template>
-    <div>test</div>
+    <div>test
+
+        <ZpButton>按钮</ZpButton>
+    </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue';
+
+import { ZpButton } from 'zp-element-plus';
 </script>
 
 <style scoped lang="less"></style>
+
--- functionExample/vite.config.js
+++ functionExample/vite.config.js
@@ -6,7 +6,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
     base: '/vue3',
-    publicPath: '/admin',
+    publicPath: '/vue3',
     server: {
         port: 8084,
         cors: true,
