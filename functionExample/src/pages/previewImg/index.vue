0d6ea6338f142232268f4f53136fdeb4c893304f
zhangping <2425238116@qq.com>
2024-09-25 16:44:59 +0800

    feat: 优化预览图片

--- functionExample/src/pages/previewImg/index.vue
+++ functionExample/src/pages/previewImg/index.vue
@@ -1,13 +1,20 @@
 <template>
-    <div>
-        <div v-viewer>
-            <img v-for="src in images" :key="src" :src="src" style="margin-right: 10px" />
-        </div>
+    <div v-viewer="{ movable: true }">
+        <img v-for="src in images" :key="src" :src="src" style="margin-right: 10px" />
     </div>
 </template>
 
 <script setup>
-const images = ['https://picsum.photos/200/200', 'https://picsum.photos/300/200', 'https://picsum.photos/250/200'];
+const images = [
+    'https://file.iviewui.com/images/image-demo-10.jpg',
+    'https://file.iviewui.com/images/image-demo-11.jpg',
+    'https://file.iviewui.com/images/image-demo-12.jpg'
+];
 </script>
 
-<style scoped></style>
+<style scoped>
+img {
+    width: 100px;
+    height: 100px;
+}
+</style>
