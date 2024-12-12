d4cf1b02107d3cc4481364fb538a8e95437cbcfb
zhangping <2425238116@qq.com>
2024-12-12 16:21:25 +0800

    test: 11

--- functionExample/src/pages/test/index.vue
+++ functionExample/src/pages/test/index.vue
@@ -1,44 +1,11 @@
 <template>
-    <div>test</div>
+    <div>test11</div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue';
-import { Modal } from 'view-ui-plus';
 
-const timer = ref();
-
-const checkUpdate = async () => {
-    let res = await fetch('/vue3/versionData.json', {
-        headers: {
-            'Cache-Control': 'no-cache'
-        }
-    }).then((r) => r.json());
-    if (!localStorage.getItem('demo_version')) {
-        localStorage.setItem('demo_version', res.version);
-    } else {
-        console.log('新版本时间戳', res.version);
-        if (res.version != localStorage.getItem('demo_version')) {
-            localStorage.setItem('demo_version', res.version);
-            Modal.confirm({
-                title: '检测到新版本，是否更新？',
-                content: '新版本内容：' + res.content,
-                okText: '更新',
-                cancelText: '取消',
-                onOk: () => {
-                    window.location.reload();
-                }
-            });
-        }
-    }
-};
-
-onMounted(() => {
-    clearInterval(timer.value);
-    timer.value = setInterval(async () => {
-        checkUpdate();
-    }, 3000);
-});
+onMounted(() => {});
 </script>
 
 <style lang="less" scoped></style>
