54a97c292e0d07c52be589ce5512665e0d48816a
zhangping <2425238116@qq.com>
2025-03-29 16:20:58 +0800

    feat: 高德-自定义地图

---
+++ functionExample/src/pages/gaodeMap/customMap.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="box" id="customMap-mapContainer"></div>
+</template>
+
+<script setup>
+import { onMounted, onUnmounted } from 'vue';
+
+let map = null; // 地图实例
+
+// 初始化-地图
+const initMap = () => {
+    map = new AMap.Map('customMap-mapContainer', {
+        zoom: 10, // 缩放级别
+        center: [120.2126, 30.290851], // 中心点坐标
+        scrollWheel: true, // 是否滚轮缩放
+        mapStyle: 'amap://styles/eed6bd92ef6e537a61874677487d66ca' //设置地图的显示样式
+    });
+};
+
+onMounted(() => {
+    initMap();
+});
+onUnmounted(() => {
+    map.destroy();
+});
+</script>
+
+<style lang="less" scoped>
+.box {
+    width: 600px;
+    height: 400px;
+    border: 1px solid #000;
+}
+</style>
--- functionExample/src/pages/gaodeMap/index.vue
+++ functionExample/src/pages/gaodeMap/index.vue
@@ -8,14 +8,22 @@
     <div class="pt-20 pl-20">
         <Base />
         <hr class="mt-50 mb-50" />
+
         <!--画线-->
         <PolylineDemo />
         <hr class="mt-50 mb-50" />
+
         <!-- 批量打点 -->
         <BatchMarker />
         <hr class="mt-50 mb-50" />
+
         <!-- 根据地点在地图上搜索，并定位标记画圆，可拖拽 -->
         <SearchAdress />
+        <hr class="mt-50 mb-50" />
+
+        <!-- 自定义地图 -->
+        <CustomMap />
+        <hr class="mt-50 mb-50" />
     </div>
 </template>
 
@@ -24,6 +32,7 @@ import Base from './base';
 import PolylineDemo from './polylineDemo';
 import BatchMarker from './batchMarker';
 import SearchAdress from './searchAdress';
+import CustomMap from './customMap';
 </script>
 
 <style lang="less" scoped></style>
