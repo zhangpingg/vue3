67fedf7a2b21f038405bde47d97d3577502b5ebb
zhangping <2425238116@qq.com>
2025-07-25 14:29:10 +0800

    feat: 优化

--- template/config/proxy.js
+++ template/config/proxy.js
@@ -1,6 +1,6 @@
 const envApi = {
     test: {
-        api: 'http://10.1.13.22', // 域名+
+        api: 'http://10.1.13.22', // 域名+, 正常来讲应该要有端口的，只不过这里刚好没有
     },
     prod: {
         api: 'https://quality.cy.com:4433', // 域名+，端口+
