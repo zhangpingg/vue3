56492be00ca2c8c4fe5297046d61a7c947e4a1f3
zhangping <2425238116@qq.com>
2025-07-25 14:01:25 +0800

    feat: 优化

--- template/config/proxy.js
+++ template/config/proxy.js
@@ -1,9 +1,9 @@
 const envApi = {
     test: {
-        api: 'http://10.1.13.22:8080',
+        api: 'http://10.1.13.22', // 域名+
     },
     prod: {
-        api: 'https://quality.cy.com:4433',
+        api: 'https://quality.cy.com:4433', // 域名+，端口+
     },
     environment: 'test',
 };
